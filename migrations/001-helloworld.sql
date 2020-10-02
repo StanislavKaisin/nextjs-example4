3f254d2091ee801590f8f59d0df2df1ab99a4113
StanislavKaisin <8050361@gmail.com>
2020-10-02 20:57:06 +0300

    initial commit

---
+++ migrations/001-helloworld.sql
@@ -0,0 +1,9 @@
+--FAQ
+CREATE TABLE FAQ (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  question TEXT,
+  answer TEXT,
+  createDate DATE
+)
+
+INSERT INTO FAQ (question, answer, createDate) values ()
